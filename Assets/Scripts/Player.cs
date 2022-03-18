b7aa255eed99d68055e995ff6dd2f60b789b21b1
Amelia Roberts <amemaeroberts@gmail.com>
2022-03-18 15:11:26 -0700

    Add ability for player prefab to move

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector2 oldPos, newPos;
+    public float speed = 5;
+
+    private void Update()
+    {
+        oldPos = transform.position;
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            Debug.Log("Read right arrow");
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+
+    }
+}
