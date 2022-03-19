76b405ff7e9c230787035f30d7be69906ab96bbf
Amelia Roberts <amemaeroberts@gmail.com>
2022-03-19 14:06:17 -0700

    Have the projectile instantiate out of bounds and fire from the player when the mouse is clicked

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,9 +6,18 @@ public class Player : MonoBehaviour
 {
     Vector2 oldPos, newPos;
     public float speed = 5;
+    public Projectile prefabProj;
+    private Projectile proj;
+
+    private void Start()
+    {
+        //Make starting position a global variable
+        proj = Instantiate(prefabProj, new Vector2(-1000, -1000), Quaternion.identity);
+    }
 
     private void Update()
     {
+        //Movement
         oldPos = transform.position;
         if (Input.GetAxisRaw("Horizontal") < 0)
         {
@@ -27,8 +36,24 @@ public class Player : MonoBehaviour
 
         if (Input.GetAxisRaw("Vertical") < 0)
         {
-            transform.Translate(Vector3.down * speed * Time.deltaTime);
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
         }
 
+        //Attacking
+        //0 here represents primary mouse button
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Move the projectile onto the player
+            proj.transform.position = new Vector2(transform.position.x, transform.position.y);
+            proj.IsActive = true;
+            //Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        /*Vector2 mousePos = Input.mousePosition;
+        //Converts the players position into pixel units so it can be compared against mousePosition
+        Vector2 playerPixelPos = Camera.main.WorldToScreenPoint(transform.position);*/
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    private bool _isActive = true;
+    private bool _isActive = false;
     public float speed = 5;
 
     void Update()
@@ -12,6 +12,17 @@ public class Projectile : MonoBehaviour
         if (_isActive)
         {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
+            //Sample code to deactive projectile when the player cannot see it
+            if (transform.position.x > 100 || transform.position.y > 100 || transform.position.x < -100 || 
+                transform.position.y < -100)
+            {
+                _isActive = false;
+            }
         }
+    }
+
+   public bool IsActive
+    {
+        set { _isActive = value; }
     }
 }
