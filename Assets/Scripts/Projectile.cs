6b1aea640929d942bb6792c699c4f8e445d32414
Amelia Roberts <amemaeroberts@gmail.com>
2022-03-19 13:45:59 -0700

    Set up projectile with a script to have it move in a sample direction

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool _isActive = true;
+    public float speed = 5;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (_isActive)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
     }
 }
