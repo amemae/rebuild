c93d2a19ba25374337ab24ee2ca27299d350785c
Amelia Roberts <amemaeroberts@gmail.com>
2022-03-20 15:37:23 -0700

    Add timer for enemy attacks

--- Assets/Scripts/Actors/Enemangle.cs
+++ Assets/Scripts/Actors/Enemangle.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Enemangle : Actor
 {
+    public float _shotsPerSecond;
+    private float _shotClock = 0;
+    private void FixedUpdate()
+    {
+        ++_shotClock;
+        //50 represents the number of times FixedUpdate is called per second (with a framerate of at least 50)
+        if (_shotClock >= _shotsPerSecond * 50)
+        {
+            _shotClock = 0;
+            ShootProjectile();
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         _hp -= other.GetComponent<Projectile>().Damage;
